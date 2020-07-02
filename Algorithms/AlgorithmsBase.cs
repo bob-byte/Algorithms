ac7372a9f49f82ef388eb970a1aaff042e742f18
bob-byte <bac.bogdan222@gmail.com>
2020-07-02 12:11:50 +0300

    Добавьте файлы проекта.

---
+++ Algorithms/AlgorithmsBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+    public class AlgorithmsBase<T> where T : IComparable
+    {
+        public List<T> Items { get; set; } = new List<T>();
+
+        protected void Swop(Int32 positionA, Int32 positionB)
+        {
+            if(positionA < Items.Count && positionB < Items.Count)
+            {
+                T temp = Items[positionA];
+                Items[positionA] = Items[positionB];
+                Items[positionB] = temp;
+            }
+        }
+
+        public virtual void Sort()
+        {
+            Items.Sort();
+        }
+    }
+}
---
+++ AlgorithmsTests/BubbleSortTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Tests
+{
+    [TestClass()]
+    public class BubbleSortTests
+    {
+        [TestMethod()]
+        public void SortTest()
+        {
+            BubbleSort<Int32> bubble = new BubbleSort<Int32>();
+
+            Random rnd = new Random();
+            List<Int32> list = new List<int>();
+            for (int i = 0; i < 1000; i++)
+            {
+                list.Add(rnd.Next(0, 100));
+            }
+            bubble.Items.AddRange(list);
+            list.Sort();
+
+            bubble.Sort();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Assert.AreEqual(list[i], bubble.Items[i]);
+            }
+        }
+    }
+}
